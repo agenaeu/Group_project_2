fc908f21e002e500cd8d72a84bcd1138885c3f6f
GeoQer <40548603+GeoQer@users.noreply.github.com>
2018-11-17 14:02:31 -0700

    Updated schema and added seeds file.

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,23 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+-- Create the database task_saver_db and specified it for use.
+DROP DATABASE IF EXISTS sushi_db;
+CREATE DATABASE sushi_db;
+USE sushi_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+-- Create the table tasks.
+CREATE TABLE orders
+(
+id int NOT NULL AUTO_INCREMENT,
+rollName varchar(255) NOT NULL,
+quantity int NOT NULL,
+tableNumber int NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE menu
+(
+id int NOT NULL AUTO_INCREMENT,
+price int NOT NULL,
+rollName varchar(255) NOT NULL,
+category varchar(255) NOT NULL,
+PRIMARY KEY (id)
+);
