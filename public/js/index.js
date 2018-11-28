a3682325beed2243a954a12d733fa64a9903a1d8
Agenaeu <sdrice18@gmail.com>
2018-11-27 21:38:23 -0700

    working array for post

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,21 @@
 $(document).ready(function () {
+    
 
     let tableNum = Math.floor(Math.random() * 4) + 1;
     var tableOneOrder = [];
+ 
 
-    function SushiOrder(Roll, howMany, tableNumber){
-        this.Roll = rollName;
-        this.howMany = quantity;
-        this.tableNumber = tableNum;
-    }
-    function addOrder(Roll, howMany, tableNumber){
-        var one = new SushiOrder(Roll, howMany, tableNumber);
-        tableOneOrder.push(one);
-    }
+
+
     
 
    
    
 
     $(document).on("click", "#orderSubmit", newOrder);
+   
     event.preventDefault();
-    console.log()
+  
   
 
 
@@ -34,38 +30,75 @@ $(document).ready(function () {
     $('#s_1').on('change', function () {
         var value = $(this).val();
         $('#res1').html("TUNA " + value);
+        $.get("/api/food/1", function(results){
+           // console.log(results);
+            tableOneOrder.push(results);
+            console.log(tableOneOrder);
+        });
     });
 
     $('#s_2').on('change', function () {
         var value = $(this).val();
         $('#res2').html("TUNA BELLY " + value);
+        $.get("/api/food/2", function(results){
+            // console.log(results);
+             tableOneOrder.push(results);
+             console.log(tableOneOrder);
+         });
     });
 
     $('#s_3').on('change', function () {
         var value = $(this).val();
         $('#res3').html("YELLOWTAIL " + value);
+        $.get("/api/food/3", function(results){
+            // console.log(results);
+             tableOneOrder.push(results);
+             console.log(tableOneOrder);
+         });
     });
 
     $('#s_4').on('change', function () {
         var value = $(this).val();
         $('#res4').html("SALMON " + value);
+        $.get("/api/food/4", function(results){
+            // console.log(results);
+             
+             
+             tableOneOrder.push(results);
+             console.log(tableOneOrder);
+         });
     });
 
 
     $('#s_5').on('change', function () {
         var value = $(this).val();
         $('#res5').html("OCTOPUS " + value);
+        $.get("/api/food/5", function(results){
+            // console.log(results);
+             tableOneOrder.push(results);
+             console.log(tableOneOrder);
+         });
     });
 
     $('#s_6').on('change', function () {
         var value = $(this).val();
         $('#res6').html("FRESH WATER EEL " + value);
+        $.get("/api/food/6", function(results){
+            // console.log(results);
+             tableOneOrder.push(results);
+             console.log(tableOneOrder);
+         });
     });
 
 
     $('#s_7').on('change', function () {
         var value = $(this).val();
         $('#res7').html("SALT WATER EEL " + value);
+        $.get("/api/food/7", function(results){
+            // console.log(results);
+             tableOneOrder.push(results);
+             console.log(tableOneOrder);
+         });
     });
 
 
@@ -73,6 +106,11 @@ $(document).ready(function () {
     $('#s_8').on('change', function () {
         var value = $(this).val();
         $('#res8').html("SEA URCHIN " + value);
+        $.get("/api/food/8", function(results){
+            // console.log(results);
+             tableOneOrder.push(results);
+             console.log(tableOneOrder);
+         });
     });
 
 
@@ -80,30 +118,55 @@ $(document).ready(function () {
     $('#s_9').on('change', function () {
         var value = $(this).val();
         $('#res9').html("SHRIMP " + value);
+        $.get("/api/food/9", function(results){
+            // console.log(results);
+             tableOneOrder.push(results);
+             console.log(tableOneOrder);
+         });
     });
 
 
     $('#s_10').on('change', function () {
         var value = $(this).val();
         $('#res10').html("SWEET SHRIMP " + value);
+        $.get("/api/food/10", function(results){
+            // console.log(results);
+             tableOneOrder.push(results);
+             console.log(tableOneOrder);
+         });
     });
 
 
     $('#s_11').on('change', function () {
         var value = $(this).val();
         $('#res11').html("SMOKED SALMON " + value);
+        $.get("/api/food/11", function(results){
+            // console.log(results);
+             tableOneOrder.push(results);
+             console.log(tableOneOrder);
+         });
     });
 
 
     $('#s_12').on('change', function () {
         var value = $(this).val();
         $('#res12').html("VEGI VEGI " + value);
+        $.get("/api/food/12", function(results){
+            // console.log(results);
+             tableOneOrder.push(results);
+             console.log(tableOneOrder);
+         });
     });
 
 
     $('#s_13').on('change', function () {
         var value = $(this).val();
         $('#res13').html("RAINBOW " + value);
+        $.get("/api/food/13", function(results){
+            // console.log(results);
+             tableOneOrder.push(results);
+             console.log(tableOneOrder);
+         });
     });
 
 
@@ -111,39 +174,74 @@ $(document).ready(function () {
     $('#s_14').on('change', function () {
         var value = $(this).val();
         $('#res14').html("CATERPILLAR " + value);
+        $.get("/api/food/14", function(results){
+            // console.log(results);
+             tableOneOrder.push(results);
+             console.log(tableOneOrder);
+         });
     });
 
 
     $('#s_15').on('change', function () {
         var value = $(this).val();
         $('#res15').html("SOFT SHELL CRAB " + value);
+        $.get("/api/food/15", function(results){
+            // console.log(results);
+             tableOneOrder.push(results);
+             console.log(tableOneOrder);
+         });
     });
 
 
     $('#s_16').on('change', function () {
         var value = $(this).val();
         $('#res16').html("FUTOMAKI " + value);
+        $.get("/api/food/16", function(results){
+            // console.log(results);
+             tableOneOrder.push(results);
+             console.log(tableOneOrder);
+         });
     });
 
     $('#s_17').on('change', function () {
         var value = $(this).val();
         $('#res17').html("DOUBLE DRAGON " + value);
+        $.get("/api/food/17", function(results){
+            // console.log(results);
+             tableOneOrder.push(results);
+             console.log(tableOneOrder);
+         });
     });
 
 
     $('#s_18').on('change', function () {
         var value = $(this).val();
         $('#res18').html("SAKE KANI " + value);
+        $.get("/api/food/18", function(results){
+            // console.log(results);
+             tableOneOrder.push(results);
+             console.log(tableOneOrder);
+         });
     });
 
     $('#s_19').on('change', function () {
         var value = $(this).val();
         $('#res19').html("BIG EYE DIVER " + value);
+        $.get("/api/food/19", function(results){
+            // console.log(results);
+             tableOneOrder.push(results);
+             console.log(tableOneOrder);
+         });
     });
 
     $('#s_20').on('change', function () {
         var value = $(this).val();
         $('#res20').html("TEMPURA CRUNCHY " + value);
+        $.get("/api/food/20", function(results){
+            // console.log(results);
+             tableOneOrder.push(results);
+             console.log(tableOneOrder);
+         });
     });
 
 
@@ -151,11 +249,21 @@ $(document).ready(function () {
     $('#s_21').on('change', function () {
         var value = $(this).val();
         $('#res21').html("AHI TUNA TATAKI " + value);
+        $.get("/api/food/21", function(results){
+            // console.log(results);
+             tableOneOrder.push(results);
+             console.log(tableOneOrder);
+         });
     });
 
     $('#s_22').on('change', function () {
         var value = $(this).val();
         $('#res22').html("POKI POKI " + value);
+        $.get("/api/food/22", function(results){
+            // console.log(results);
+             tableOneOrder.push(results);
+             console.log(tableOneOrder);
+         });
     });
 
 
--- routes/foodApiRoutes.js
+++ routes/foodApiRoutes.js
@@ -1,3 +1,5 @@
+/* var express = require("express");
+var router = express.Router(); */
 var db = require("../models");
 
 module.exports = function (app) {
@@ -5,6 +7,18 @@ module.exports = function (app) {
   app.get("/api/food", function (req, res) {
     db.Food.findAll({}).then(function (dbFood) {
       res.json(dbFood);
+    });
+  });
+
+  app.get("/api/food/:id", function (req, res) {
+ 
+    db.food.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbFood) {
+      res.json(dbFood);
+      console.log(dbFood);
     });
   });
 
